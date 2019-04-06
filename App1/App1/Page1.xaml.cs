e6a3e8608e2797c784b814eb13b82eec20c45e1a
Alex Cherry <G00347106@gmit.ie>
2019-04-06 23:03:36 +0100

    Update Page1.xaml.cs

--- App1/App1/Page1.xaml.cs
+++ App1/App1/Page1.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +9,8 @@ namespace App1
 {
     public partial class Page1 : ContentPage
     {
-        bool bW, bH;
 
+        int counter = 0;
         public Page1()
         {
             InitializeComponent();
@@ -20,102 +20,18 @@ namespace App1
 
         private void SetupDefaults()
         {
-            //ctrl + . generate method
-            bW = false;
-            bH = false;
-            btnBodyFatPage.IsEnabled = false;
-            //show image on screen, chart.png
-            var assembly = typeof(MainPage);
-            string fileName = "App1.Assets.Images.chart.png";
-            chart.Source = ImageSource.FromResource(fileName, assembly);
+            
+            
         }
 
-        private void btnCalculate_Clicked(object sender, EventArgs e)
+        private void ClickCounter_Clicked(object sender, EventArgs e)
         {
-            //calculate BMI for h/w entered by user
-            //include checks for h/w
-            double bmi;
-            bmi = (Convert.ToDouble(entWeight.Text) / (Convert.ToDouble(entHeight.Text) * Convert.ToDouble(entHeight.Text)));
-            lblAnswer.Text = "Your BMI is : " + bmi.ToString("0.00");
-            btnBodyFatPage.IsEnabled = true;
-        }
-
-        private void entWeight_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (entWeight.Text == "")
-            {
-                btnCalculate.IsEnabled = false;
-                lblAnswer.Text = "Your BMI is : ";
-                bW = false;
-
-            }
-            else
-            {
-                bW = true;
-
-            }
-
-            //check other bool
-
-            if (bW == true)
-
-            {
-                if (bH == true) btnCalculate.IsEnabled = true;
-
-            }
-        }
-
-        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
-        {
-            ResetFieldsGeneral();
-        }
-
-        private void ResetFieldsGeneral()
-        {
-            bW = bH = false;
-            entHeight.Text = "";
-            entWeight.Text = "";
-            btnCalculate.IsEnabled = false;
-        }
-
-        private void btnBodyFatPage_Clicked(object sender, EventArgs e)
-        {
-            //navigates to body fat page
-
-            Navigation.PushAsync(new Page1());
-        }
-
-        private void entHeight_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-            //check entry boxes are full
-            if (entHeight.Text == "")
-            {
-                btnCalculate.IsEnabled = false;
-                lblAnswer.Text = "Your BMI is : ";
-                bH = false;
-
-            }
-            else
-            {
-                bH = true;
-
-            }
-
-            //check other bool
-
-            if (bH == true)
-
-            {
-                if (bW == true) btnCalculate.IsEnabled = true;
-
-            }
-
-
+            //when the click counter main button is clicked, this will activate the timer
+            counter++;
+            
+            
+            label1.Text = "Times Clicked : " + counter;
         }
     }
-
-
-
 
 }
