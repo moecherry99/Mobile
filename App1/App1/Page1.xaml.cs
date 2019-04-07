b92b85fb60abe13b6d163ae58cf1f7dfd1ab77d1
Alex Cherry <G00347106@gmit.ie>
2019-04-07 13:17:08 +0100

    Update Page1.xaml.cs

--- App1/App1/Page1.xaml.cs
+++ App1/App1/Page1.xaml.cs
@@ -1,37 +1,68 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
 
-namespace App1
-{
-    public partial class Page1 : ContentPage
+    namespace App1
     {
-
-        int counter = 0;
-        public Page1()
+        public partial class Page1 : ContentPage
         {
-            InitializeComponent();
-            SetupDefaults();
 
+            private System.Timers.Timer buttonTimer;
+            int timer = 0;
+        
+            int counter = 0;
+        
+            public Page1()
+            {
+                InitializeComponent();
+                SetupDefaults();
+            
+
+            }
+
+            private void SetupDefaults()
+            {
+            
+            
+            }
+            private void Game1Starter_Clicked(object sender, EventArgs e)
+            {
+                clickCounter.IsEnabled = true;
+
+                //this simply sets all of the values on the page back to what they
+                //were before the Start Game 1 button was pressed
+                counter = 0;
+                label1.Text = "Times Clicked : " + counter;
+                label2.Text = "";
+            }
+
+            private void ClickCounter_Clicked(object sender, EventArgs e)
+            {
+                //when the click counter main button is clicked, this will activate the timer
+                counter++;
+
+            
+          
+            
+            
+                label1.Text = "Times Clicked : " + counter;
+
+                if (counter == 20)
+                {
+                
+                    clickCounter.IsEnabled = false;
+                
+                    label2.Text = "You Clicked the button " + counter + " times in 10 seconds";
+
+
+
+                }
+            }
+
+       
         }
 
-        private void SetupDefaults()
-        {
-            
-            
-        }
-
-        private void ClickCounter_Clicked(object sender, EventArgs e)
-        {
-            //when the click counter main button is clicked, this will activate the timer
-            counter++;
-            
-            
-            label1.Text = "Times Clicked : " + counter;
-        }
     }
-
-}
