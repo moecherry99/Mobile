0e3f32178eb993168f8edc45fbe0d5e0386eeac7
Alex Cherry <moecherry2@gmail.com>
2019-04-17 22:08:09 +0100

    Update Page2.xaml.cs

--- App1/App1/Page2.xaml.cs
+++ App1/App1/Page2.xaml.cs
@@ -26,21 +26,24 @@ namespace App1
 
         private void EntText_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+            //if text is in the box, the confirm button can be clicked
             if(entText.ToString()==textToEnter.ToString())
             {
                 gameConfirm.IsEnabled = true;
                 
-                
             }
+
         }
 
         private void Game2Starter_Clicked(object sender, EventArgs e)
         {
+            //random array of strings that are chosen to type
             string[] text = { "Android", "Apple", "Samsung", "Huawei", "Sony" };
             entText.IsEnabled = true;
             textToEnter.Text = text[new Random().Next(0, text.Length)];
             gameConfirm.IsEnabled = true;
+
+            //stopwatch timer starts
             mStopWatch.Start();
         }
 
@@ -48,26 +51,31 @@ namespace App1
         {
             if (entText.Text == textToEnter.Text)
             {
+                //if it is correct when confirm button is clicked, the timer stops 
+                //the users time will be displayed if it is correct
                 mStopWatch.Stop();
-                long elapsed = mStopWatch.ElapsedMilliseconds;
+                double elapsed = (double)mStopWatch.ElapsedMilliseconds / 1000;
                 time.Text = elapsed.ToString();
                 mStopWatch.Reset();
                 ifChecker.Text = "It is correct!";
-                time.Text = "Time to enter string: "+time.Text+" milliseconds";
+                time.Text = "Time to enter string: "+time.Text+" seconds";
                 gameConfirm.IsEnabled = false;
             }
             else
             {
+                //timer does not stop if entry is incorrect
                 ifChecker.Text = "It is not correct!";
             }
+
         }
 
         private void Game2Rules_Clicked(object sender, EventArgs e)
         {
-            //this is to navigate to the second game page
+            //this is to navigate to the second pages game rules
             Navigation.PushAsync(new Game2Rules());
 
         }
+
     }
 
 }
